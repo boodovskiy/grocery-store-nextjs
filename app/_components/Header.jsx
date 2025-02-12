6c64293fdebb2b38226eaa432620053f2b7c37b8
boodovskiy <dzak1987@mail.ru>
2025-02-12 09:32:58 +0200

    Add cart button and login button

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -1,10 +1,11 @@
-import { LayoutGrid, Search } from 'lucide-react'
+import { LayoutGrid, Search, ShoppingBag } from 'lucide-react'
 import Image from 'next/image'
 import React from 'react'
+import { Button } from '@/components/ui/button'
 
 const Header = () => {
   return (
-    <div className='p-5 shadow-md'>
+    <div className='flex justify-between p-5 shadow-md'>
         <div className='flex items-center gap-8'>
             <Image 
                 src='/logo.png'
@@ -12,13 +13,16 @@ const Header = () => {
                 width={150}
                 height={100}
             />
-             <h2 className='flex gap-2 items-center borderd rounded-full p-2 px-10 bg-slate-200'><LayoutGrid className='h-5 w-5'/> Category</h2>
+             <h2 className='hidden md:flex gap-2 items-center borderd rounded-full p-2 px-10 bg-slate-200'><LayoutGrid className='h-5 w-5'/> Category</h2>
             <div className='md:flex gap-3 items-center border rounded-full p-2 px-5 hidden'>
                 <Search />
                 <input type='text' placeholder='Search' className='outline-none'/>
             </div>
         </div>
-           
+        <div className='flex gap-5 items-center'>
+          <h2 className='flex gap-2 items-center text-lg'><ShoppingBag /> 0</h2>
+          <Button>Login</Button>
+        </div>
     </div>
   )
 }
