ca48600e6cb4ddf0a22632865944c21c358799bd
boodovskiy <dzak1987@mail.ru>
2025-02-13 10:02:26 +0200

    Add DropdownMenu

--- app/_components/Header.jsx
+++ app/_components/Header.jsx
@@ -2,6 +2,7 @@ import { LayoutGrid, Search, ShoppingBag } from 'lucide-react'
 import Image from 'next/image'
 import React from 'react'
 import { Button } from '@/components/ui/button'
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuItem } from '@/components/ui/dropdown-menu'
 
 const Header = () => {
   return (
@@ -13,7 +14,23 @@ const Header = () => {
                 width={150}
                 height={100}
             />
-             <h2 className='hidden md:flex gap-2 items-center borderd rounded-full p-2 px-10 bg-slate-200'><LayoutGrid className='h-5 w-5'/> Category</h2>
+            
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <h2 className='hidden md:flex gap-2 items-center borderd rounded-full p-2 px-10 bg-slate-200 cursor-pointer'>
+                  <LayoutGrid className='h-5 w-5'/> Category
+                </h2>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuLabel>Browse Category</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Profile</DropdownMenuItem>
+                <DropdownMenuItem>Billing</DropdownMenuItem>
+                <DropdownMenuItem>Team</DropdownMenuItem>
+                <DropdownMenuItem>Subscription</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+
             <div className='md:flex gap-3 items-center border rounded-full p-2 px-5 hidden'>
                 <Search />
                 <input type='text' placeholder='Search' className='outline-none'/>
