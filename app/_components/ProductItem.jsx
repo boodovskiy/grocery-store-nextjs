e762681bbaa1bb34dc2e1252bd03e815d800935e
boodovskiy <dzak1987@mail.ru>
2025-02-20 10:25:56 +0200

    Add discounted price, finalize styling

--- app/_components/ProductItem.jsx
+++ app/_components/ProductItem.jsx
@@ -5,7 +5,9 @@ import React from 'react'
 
 const ProductItem = ({product}) => {
   return (
-    <div className='p-2 md:p-6 flex flex-col items-center justify-center gap-3 border rounded-lg'>
+    <div className='p-2 md:p-6 flex flex-col items-center justify-center gap-3 border rounded-lg 
+        hover:scale-105 hover:shadow-md transition-all ease-in-out cursor-pointer'
+    >
         <Image src={STRAPI_BASE_URL + product.images[0]?.url}
             width={500}
             height={200}
@@ -13,7 +15,12 @@ const ProductItem = ({product}) => {
             className='h-[200px] w-[200px]'
         />
         <h2 className='font-bold text-lg'>{product.name}</h2>
-        <h2 className='font-bold'>${product.mrp}</h2>
+        <div className='flex gap-3'>
+            {product.sellingPrice &&
+                <h2 className='font-bold text-lg'>${product.sellingPrice}</h2>
+            }
+            <h2 className={`font-bold text-lg ${product.sellingPrice && 'line-through text-gray-500'}`}>${product.mrp}</h2>
+        </div>
         <Button variant='outline'
             className="text-primary hover:text-white hover:bg-primary"
         >
