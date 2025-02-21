56860823d5cf91c703bc37da62db78367f98ca71
boodovskiy <dzak1987@mail.ru>
2025-02-21 11:36:44 +0200

    Add footer

--- app/page.js
+++ app/page.js
@@ -2,6 +2,8 @@ import GlobalApi from "./_utils/GlobalApi";
 import Slider from "./_components/Slider";
 import CategoryList from "./_components/CategoryList";
 import ProductList from "./_components/ProductList";
+import Image from "next/image";
+import Footer from "./_components/Footer";
 
 export default async function Home() {
   const sliderList = await GlobalApi.getSliders();
@@ -13,6 +15,8 @@ export default async function Home() {
       <Slider sliderList={sliderList} />
       <CategoryList categoryList={categoryList}/>
       <ProductList productList={productList}/>
+      <Image src='/banner.png' width={1000} height={300} alt="banner" className="w-full h-[400px] object-contain"/>
+      <Footer />
     </div>
   );
 }
