de94057cc6c32cac21286f9235da88097131c7f5
boodovskiy <dzak1987@mail.ru>
2025-02-27 09:13:51 +0200

    Fix issue with key for ProductItem

--- app/_components/ProductList.jsx
+++ app/_components/ProductList.jsx
@@ -7,7 +7,7 @@ const ProductList = ({productList}) => {
         <h2 className='text-green-600 font-bold text-2xl'>Our Popular Products</h2>
         <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
             {productList.map((product, index)=> index<8 && (
-                <ProductItem product={product} />
+                <ProductItem product={product} key={product.id || index} />
             ))}
         </div>
     </div>
