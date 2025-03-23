696f20ea6688b5232eb82c122482033f64298a0a
boodovskiy <dzak1987@mail.ru>
2025-03-23 16:06:14 +0200

    Fix empty image in MyOrderItem (orderHistory), add placeholder

--- app/_components/MyOrderItem.jsx
+++ app/_components/MyOrderItem.jsx
@@ -3,13 +3,17 @@ import Image from "next/image"
 import { STRAPI_BASE_URL } from '@/config'
 
 function MyOrderItem({orderItem}) {
+  const imageUrl = orderItem.product?.images[0]?.url 
+  ? `${STRAPI_BASE_URL}${orderItem.product?.images[0]?.url}`
+  : 'https://picsum.photos/200';
+
   return (
     <div className='grid grid-cols-5 w-[55%] mt-3 mb-3 items-center border-b-2'>
         <Image 
-            src={STRAPI_BASE_URL + orderItem.product?.images[0]?.url} 
+            src={imageUrl} 
             width={80} 
             height={80} 
-            alt={orderItem.product?.name}
+            alt={orderItem.product?.name ? orderItem.product?.name : 'image'}
             className='bg-gray-100 p-5 rounded-md border'
         />
         <div className='col-span-2'>
