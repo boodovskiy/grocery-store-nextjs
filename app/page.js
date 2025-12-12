d08e249d3da9d823515435ecac6a4586ab0681a5
boodovskiy <dzak1987@mail.ru>
2025-12-12 12:27:09 +0200

    prettier

--- app/page.js
+++ app/page.js
@@ -5,19 +5,25 @@ import ProductList from "./_components/ProductList";
 import Image from "next/image";
 import Footer from "./_components/Footer";
 
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
 
 export default async function Home() {
   const sliderList = await GlobalApi.getSliders();
   const categoryList = await GlobalApi.getCategoryList();
   const productList = await GlobalApi.getAllProducts();
- 
+
   return (
     <div className="p-5 md:p-10 px-16">
       <Slider sliderList={sliderList} />
-      <CategoryList categoryList={categoryList}/>
-      <ProductList productList={productList}/>
-      <Image src='/banner.png' width={1000} height={300} alt="banner" className="w-full h-[400px] object-contain"/>
+      <CategoryList categoryList={categoryList} />
+      <ProductList productList={productList} />
+      <Image
+        src="/banner.png"
+        width={1000}
+        height={300}
+        alt="banner"
+        className="w-full h-[400px] object-contain"
+      />
       <Footer />
     </div>
   );
