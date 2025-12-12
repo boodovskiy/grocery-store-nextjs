493d6992db25e8be9e0789f7e7fa6b2eebc3868b
boodovskiy <dzak1987@mail.ru>
2025-12-12 12:26:39 +0200

    chore: update Next.js to 15.5.8 and refactor tailwindcss-animate plugin usage

--- app/page.js
+++ app/page.js
@@ -5,6 +5,8 @@ import ProductList from "./_components/ProductList";
 import Image from "next/image";
 import Footer from "./_components/Footer";
 
+export const dynamic = 'force-dynamic';
+
 export default async function Home() {
   const sliderList = await GlobalApi.getSliders();
   const categoryList = await GlobalApi.getCategoryList();
